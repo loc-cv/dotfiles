b305917145060e2e805b0cbf45156da4a15f2b53
loc-cv <donbb6824@gmail.com>
2022-04-24 22:48:04 +0700

    Wrap nvim_set_hl to make it easier to use

--- nvim/.config/nvim/lua/core/highlights.lua
+++ nvim/.config/nvim/lua/core/highlights.lua
@@ -1,4 +1,5 @@
-local hi = vim.api.nvim_set_hl
+local utils = require "core.utils"
+local hi = utils.hi
 
 local colors = {
   -- cursorline
@@ -60,61 +61,61 @@ vim.g.vscode_style = "dark"
 vim.cmd [[colorscheme vscode]]
 
 -- CursorLine
-hi(0, "CursorLine", { bg = colors.CursorLine })
+hi("CursorLine", { bg = colors.CursorLine })
 
 -- Trailing whitespaces
 vim.g.better_whitespace_guicolor = colors.Whitespace
 
 -- Telescope
-hi(0, "TelescopeSelection", { bg = colors.TelescopeSelection })
+hi("TelescopeSelection", { bg = colors.TelescopeSelection })
 
 -- Floaterm
-hi(0, "FloatermBorder", { bg = colors.FloatermBorder })
+hi("FloatermBorder", { bg = colors.FloatermBorder })
 
 -- Statusline
-hi(0, "StatusLine", { bg = colors.StatusLine })
-hi(0, "StatusLineNC", { bg = colors.StatusLineNC })
+hi("StatusLine", { bg = colors.StatusLine })
+hi("StatusLineNC", { bg = colors.StatusLineNC })
 
 -- Git
-hi(0, "DiffAdd", { fg = colors.DiffAdd, bg = "NONE" })
-hi(0, "DiffChange", { fg = colors.DiffChange, bg = "NONE" })
-hi(0, "DiffDelete", { fg = colors.DiffDelete, bg = "NONE" })
+hi("DiffAdd", { fg = colors.DiffAdd, bg = "NONE" })
+hi("DiffChange", { fg = colors.DiffChange, bg = "NONE" })
+hi("DiffDelete", { fg = colors.DiffDelete, bg = "NONE" })
 
 -- Yank region
-hi(0, "HighlightedyankRegion", { bg = colors.HighlightedyankRegion, bold = true })
+hi("HighlightedyankRegion", { bg = colors.HighlightedyankRegion, bold = true })
 
 -- For indent_blankline only
-hi(0, "IndentBlanklineContextChar", { fg = colors.IndentBlanklineContextChar })
-hi(0, "IndentBlanklineContextStart", { fg = "NONE", sp = colors.IndentBlanklineContextStart, underline = true })
+hi("IndentBlanklineContextChar", { fg = colors.IndentBlanklineContextChar })
+hi("IndentBlanklineContextStart", { fg = "NONE", sp = colors.IndentBlanklineContextStart, underline = true })
 
 -- coc.nvim
-hi(0, "CocErrorSign", { fg = colors.CocError })
-hi(0, "CocErrorVirtualText", { fg = colors.CocError })
-hi(0, "CocErrorHighlight", { fg = colors.CocError, underline = true })
+hi("CocErrorSign", { fg = colors.CocError })
+hi("CocErrorVirtualText", { fg = colors.CocError })
+hi("CocErrorHighlight", { fg = colors.CocError, underline = true })
 
-hi(0, "CocWarningSign", { fg = colors.CocWarning })
-hi(0, "CocWarningVirtualText", { fg = colors.CocWarning })
-hi(0, "CocWarningHighlight", { fg = colors.CocWarning, underline = true })
+hi("CocWarningSign", { fg = colors.CocWarning })
+hi("CocWarningVirtualText", { fg = colors.CocWarning })
+hi("CocWarningHighlight", { fg = colors.CocWarning, underline = true })
 
-hi(0, "CocInfoSign", { fg = colors.CocInfo })
-hi(0, "CocInfoVirtualText", { fg = colors.CocInfo })
-hi(0, "CocInfoHighlight", { fg = colors.CocInfo, underline = true })
+hi("CocInfoSign", { fg = colors.CocInfo })
+hi("CocInfoVirtualText", { fg = colors.CocInfo })
+hi("CocInfoHighlight", { fg = colors.CocInfo, underline = true })
 
-hi(0, "CocHintSign", { fg = colors.CocHint })
-hi(0, "CocHintVirtualText", { fg = colors.CocHint })
-hi(0, "CocHintHighlight", { fg = colors.CocHint, underline = true })
+hi("CocHintSign", { fg = colors.CocHint })
+hi("CocHintVirtualText", { fg = colors.CocHint })
+hi("CocHintHighlight", { fg = colors.CocHint, underline = true })
 
-hi(0, "CocFadeOut", { fg = colors.CocFadeOut, underline = true })
+hi("CocFadeOut", { fg = colors.CocFadeOut, underline = true })
 
 -- nvim-window
-hi(0, "NvimWindow", { fg = colors.NvimWindowFG, bg = colors.NvimWindowBG })
+hi("NvimWindow", { fg = colors.NvimWindowFG, bg = colors.NvimWindowBG })
 
 -- vim-sandwich
-hi(0, "OperatorSandwichChange", { bg = colors.OperatorSandwichChangeBG, fg = colors.OperatorSandwichChangeFG })
+hi("OperatorSandwichChange", { bg = colors.OperatorSandwichChangeBG, fg = colors.OperatorSandwichChangeFG })
 
 -- pounce.nvim
-hi(0, "PounceMatch", { bg = colors.PounceMatchBG, fg = colors.PounceMatchFG, bold = true })
-hi(0, "PounceGap", { bg = colors.PounceGapBG, fg = colors.PounceGapFG, bold = true })
-hi(0, "PounceAccept", { bg = colors.PounceAcceptBG, fg = colors.PounceAcceptFG, bold = true })
+hi("PounceMatch", { bg = colors.PounceMatchBG, fg = colors.PounceMatchFG, bold = true })
+hi("PounceGap", { bg = colors.PounceGapBG, fg = colors.PounceGapFG, bold = true })
+hi("PounceAccept", { bg = colors.PounceAcceptBG, fg = colors.PounceAcceptFG, bold = true })
 
 return colors
--- nvim/.config/nvim/lua/core/utils.lua
+++ nvim/.config/nvim/lua/core/utils.lua
@@ -8,4 +8,12 @@ M.map = function(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, options)
 end
 
+M.hi = function(name, val, ns_id)
+  if ns_id then
+    vim.api.nvim_set_hl(ns_id, name, val)
+  else
+    vim.api.nvim_set_hl(0, name, val)
+  end
+end
+
 return M
