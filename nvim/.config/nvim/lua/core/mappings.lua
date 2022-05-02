027f1bf7e041c87a62472fb7e8d8af337a9a3237
loc-cv <donbb6824@gmail.com>
2022-05-02 21:11:40 +0700

    Minor change for CoC mappings

--- nvim/.config/nvim/lua/core/mappings.lua
+++ nvim/.config/nvim/lua/core/mappings.lua
@@ -14,11 +14,23 @@ local plugins = {
     -- Use <C-space> to trigger completion
     map("i", "<C-space>", "coc#refresh()", { expr = true })
 
+    -- Make <CR> auto-select the first completion item and notify coc.nvim to ...
+    -- ... format on enter, <CR> could be remapped by other vim plugin
+    _G.CR = function()
+      local _, autopairs = pcall(require, "nvim-autopairs")
+      if vim.fn.pumvisible() ~= 0 then
+        return vim.fn["coc#_select_confirm"]()
+      else
+        return autopairs.autopairs_cr()
+      end
+    end
+    map("i", "<cr>", "v:lua.CR()", { expr = true })
+
     -- Use <C-j> and <C-k> to navigate the completion list
     map("i", "<C-j>", 'pumvisible() ? "<C-n>" : "<Tab>"', { expr = true })
     map("i", "<C-k>", 'pumvisible() ? "<C-p>" : "<S-Tab>"', { expr = true })
 
-    -- Use `[g` and `]g` to navigate diagnostics
+    -- Use `[d` and `]d` to navigate diagnostics
     map("n", "[d", "<Plug>(coc-diagnostic-prev)")
     map("n", "]d", "<Plug>(coc-diagnostic-next)")
 
@@ -47,13 +59,16 @@ local plugins = {
     map({ "x", "n" }, "<space>fm", "<Plug>(coc-format-selected)")
 
     -- Applying code action to the selected region
-    map({ "x", "n" }, "<space>a", "<Plug>(coc-codeaction-selected)")
+    map({ "x", "n" }, "<space>ac", "<Plug>(coc-codeaction-selected)")
 
     -- Applying code action to the current buffer
     map("n", "<space>A", "<Plug>(coc-codeaction)")
 
     -- Apply autofix to problem on the current line
     map("n", "<space>qf", "<Plug>(coc-fix-current)")
+
+    -- Run the Code Lens action on the current line
+    map("n", "<space>al", "<Plug>(coc-codelens-action)")
 
     -- Map function and class text objects
     -- NOTE: Requires 'textDocument.documentSymbol' support from the language server
@@ -62,7 +77,7 @@ local plugins = {
     map({ "x", "o" }, "ic", "<Plug>(coc-classobj-i)")
     map({ "x", "o" }, "ac", "<Plug>(coc-classobj-a)")
 
-    -- Remap <C-f> and <C-b> for scroll float windows/popups
+    -- Remap <C-d> and <C-u> for scroll float windows/popups
     map({ "n", "v" }, "<C-d>", "coc#float#has_scroll() ? coc#float#scroll(1) : '<C-d>'", { expr = true, nowait = true })
     map({ "n", "v" }, "<C-u>", "coc#float#has_scroll() ? coc#float#scroll(0) : '<C-u>'", { expr = true, nowait = true })
     map(
@@ -89,18 +104,6 @@ local plugins = {
     map("n", "<space>s", "<cmd>CocFzfList symbols<cr>")
     map("n", "<space>o", "<cmd>CocFzfList outline<cr>")
     map("n", "<space>p", "<cmd>CocFzfListResume<cr>")
-
-    -- Make <CR> auto-select the first completion item and notify coc.nvim to ...
-    -- ... format on enter, <CR> could be remapped by other vim plugin
-    _G.CR = function()
-      local _, autopairs = pcall(require, "nvim-autopairs")
-      if vim.fn.pumvisible() ~= 0 then
-        return vim.fn["coc#_select_confirm"]()
-      else
-        return autopairs.autopairs_cr()
-      end
-    end
-    map("i", "<cr>", "v:lua.CR()", { expr = true })
   end,
 
   -- Easy-align
