b0c63f2e208ed6edd4ce713fe5e2509f711dfd0f
loc-cv <donbb6824@gmail.com>
2022-07-17 01:56:52 +0700

    Enhance autopairs setup

--- nvim/.config/nvim/lua/plugins/configs/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/configs/autopairs.lua
@@ -1,12 +1,51 @@
 local M = {}
 
 M.setup = function()
-  local ok, autopairs = pcall(require, 'nvim-autopairs')
+  local ok, npairs = pcall(require, 'nvim-autopairs')
   if not ok then
     return
   end
 
-  autopairs.setup({ map_cr = false, map_c_h = true, map_c_w = true })
+  local Rule = require('nvim-autopairs.rule')
+
+  npairs.setup({
+    map_cr = false,
+    map_c_h = true,
+    map_c_w = true,
+    fast_wrap = {},
+  })
+
+  -- Add spaces between parentheses
+  npairs.add_rules({
+    Rule(' ', ' '):with_pair(function(opts)
+      local pair = opts.line:sub(opts.col - 1, opts.col)
+      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+    end),
+    Rule('( ', ' )')
+      :with_pair(function()
+        return false
+      end)
+      :with_move(function(opts)
+        return opts.prev_char:match('.%)') ~= nil
+      end)
+      :use_key(')'),
+    Rule('{ ', ' }')
+      :with_pair(function()
+        return false
+      end)
+      :with_move(function(opts)
+        return opts.prev_char:match('.%}') ~= nil
+      end)
+      :use_key('}'),
+    Rule('[ ', ' ]')
+      :with_pair(function()
+        return false
+      end)
+      :with_move(function(opts)
+        return opts.prev_char:match('.%]') ~= nil
+      end)
+      :use_key(']'),
+  })
 end
 
 return M
