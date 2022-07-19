d6afa99545f2e50af379797aff1c45e78985226c
loc-cv <donbb6824@gmail.com>
2022-07-19 10:44:43 +0700

    Re-enable vim built-in plugins
    I won't go crazy with my startup time anymore, and I realize disabling
    some built-in plugins doesn't make my vim faster when I'm using it.

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -46,25 +46,25 @@ local options = {
 }
 
 M.init = function()
-  -- Disable some builtin plugins
-  local g = vim.g
-  g.loaded_2html_plugin = false
-  g.loaded_getscript = false
-  g.loaded_getscriptPlugin = false
-  g.loaded_logipat = false
-  g.loaded_netrwFileHandlers = false
-  g.loaded_netrw = false
-  g.loaded_netrwPlugin = false
-  g.loaded_netrwSettings = false
-  g.loaded_remote_plugins = false
-  g.loaded_tar = false
-  g.loaded_tarPlugin = false
-  g.loaded_zip = false
-  g.loaded_gzip = false
-  g.loaded_zipPlugin = false
-  g.loaded_vimball = false
-  g.loaded_vimballPlugin = false
-  g.loaded_rrhelper = false
+  -- -- Disable some builtin plugins
+  -- local g = vim.g
+  -- g.loaded_2html_plugin = false
+  -- g.loaded_getscript = false
+  -- g.loaded_getscriptPlugin = false
+  -- g.loaded_logipat = false
+  -- g.loaded_netrwFileHandlers = false
+  -- g.loaded_netrw = false
+  -- g.loaded_netrwPlugin = false
+  -- g.loaded_netrwSettings = false
+  -- g.loaded_remote_plugins = false
+  -- g.loaded_tar = false
+  -- g.loaded_tarPlugin = false
+  -- g.loaded_zip = false
+  -- g.loaded_gzip = false
+  -- g.loaded_zipPlugin = false
+  -- g.loaded_vimball = false
+  -- g.loaded_vimballPlugin = false
+  -- g.loaded_rrhelper = false
 
   -- Set options
   for k, v in pairs(options) do
