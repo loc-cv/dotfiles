916b82d204ddd3c1b5c79c9f8ddcb9db3bf02d1d
loc-cv <donbb6824@gmail.com>
2022-07-21 14:20:50 +0700

    Re-enable matchit and matchparen (vim builtins)

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -60,8 +60,8 @@ M.init = function()
   vim.g.loaded_vimballPlugin = 1
   vim.g.loaded_2html_plugin = 1
 
-  vim.g.loaded_matchit = 1
-  vim.g.loaded_matchparen = 1
+  -- vim.g.loaded_matchit = 1
+  -- vim.g.loaded_matchparen = 1
   vim.g.loaded_logiPat = 1
   vim.g.loaded_rrhelper = 1
 
