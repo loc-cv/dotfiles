fffa2c115ff66854649454338cb4458502bfdc31
loc-cv <donbb6824@gmail.com>
2022-08-05 06:33:36 +0700

    nah

--- nvim/.config/nvim/lua/plugins/configs/floaterm.lua
+++ nvim/.config/nvim/lua/plugins/configs/floaterm.lua
@@ -2,10 +2,10 @@ local M = {}
 
 M.setup = function()
   vim.g.floaterm_wintype = 'float'
-  vim.g.floaterm_position = 'bottomright'
+  vim.g.floaterm_position = 'topright'
   vim.g.floaterm_title = ' TERMINAL: $1/$2 '
   vim.g.floaterm_width = 0.5
-  vim.g.floaterm_height = vim.api.nvim_win_get_height(0)
+  vim.g.floaterm_height = 0.99
 end
 
 return M
