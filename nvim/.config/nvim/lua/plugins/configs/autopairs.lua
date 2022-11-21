183682f16960c5ef08580d72ec9c83557044f3fb
loc-cv <donbb6824@gmail.com>
2022-11-21 13:41:23 +0700

    Disable adding spaces between parentheses (autopairs)

--- nvim/.config/nvim/lua/plugins/configs/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/configs/autopairs.lua
@@ -17,36 +17,36 @@ M.setup = function()
   })
 
   -- Add spaces between parentheses
-  npairs.add_rules({
-    Rule(' ', ' '):with_pair(function(opts)
-      local pair = opts.line:sub(opts.col - 1, opts.col)
-      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
-    end),
-    Rule('( ', ' )')
-      :with_pair(function()
-        return false
-      end)
-      :with_move(function(opts)
-        return opts.prev_char:match('.%)') ~= nil
-      end)
-      :use_key(')'),
-    Rule('{ ', ' }')
-      :with_pair(function()
-        return false
-      end)
-      :with_move(function(opts)
-        return opts.prev_char:match('.%}') ~= nil
-      end)
-      :use_key('}'),
-    Rule('[ ', ' ]')
-      :with_pair(function()
-        return false
-      end)
-      :with_move(function(opts)
-        return opts.prev_char:match('.%]') ~= nil
-      end)
-      :use_key(']'),
-  })
+  -- npairs.add_rules({
+  --   Rule(' ', ' '):with_pair(function(opts)
+  --     local pair = opts.line:sub(opts.col - 1, opts.col)
+  --     return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+  --   end),
+  --   Rule('( ', ' )')
+  --     :with_pair(function()
+  --       return false
+  --     end)
+  --     :with_move(function(opts)
+  --       return opts.prev_char:match('.%)') ~= nil
+  --     end)
+  --     :use_key(')'),
+  --   Rule('{ ', ' }')
+  --     :with_pair(function()
+  --       return false
+  --     end)
+  --     :with_move(function(opts)
+  --       return opts.prev_char:match('.%}') ~= nil
+  --     end)
+  --     :use_key('}'),
+  --   Rule('[ ', ' ]')
+  --     :with_pair(function()
+  --       return false
+  --     end)
+  --     :with_move(function(opts)
+  --       return opts.prev_char:match('.%]') ~= nil
+  --     end)
+  --     :use_key(']'),
+  -- })
 end
 
 return M
