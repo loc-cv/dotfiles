f00955e6c26cbd21f399e77a3645ac3f71a5d37f
loc-cv <donbb6824@gmail.com>
2023-04-14 10:55:30 +0700

    Remove some nvim-tree mappings

--- nvim/.config/nvim/lua/plugins/explorer.lua
+++ nvim/.config/nvim/lua/plugins/explorer.lua
@@ -13,10 +13,6 @@ return {
       local lib = require("nvim-tree.lib")
       local view = require("nvim-tree.view")
 
-      local function collapse_all()
-        require("nvim-tree.actions.tree-modifiers.collapse-all").fn()
-      end
-
       local function edit_or_open()
         -- open as vsplit on current node
         local action = "edit"
@@ -32,24 +28,6 @@ return {
           require("nvim-tree.actions.node.open-file").fn(action, node.absolute_path)
           -- view.close() -- Close the tree if file was opened
         end
-      end
-
-      local function vsplit_preview()
-        -- open as vsplit on current node
-        local action = "vsplit"
-        local node = lib.get_node_at_cursor()
-
-        -- Just copy what's done normally with vsplit
-        if node.link_to and not node.nodes then
-          require("nvim-tree.actions.node.open-file").fn(action, node.link_to)
-        elseif node.nodes ~= nil then
-          lib.expand_or_collapse(node)
-        else
-          require("nvim-tree.actions.node.open-file").fn(action, node.absolute_path)
-        end
-
-        -- Finally refocus on tree if it was lost
-        view.focus()
       end
 
       nvimtree.setup({
@@ -69,12 +47,11 @@ return {
             custom_only = false,
             list = {
               { key = "l", action = "edit", action_cb = edit_or_open },
-              { key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
               { key = "h", action = "close_node" },
-              { key = "H", action = "collapse_all", action_cb = collapse_all },
               { key = "<C-v>", action = "vsplit" },
               { key = "<C-s>", action = "split" },
               { key = "<C-t>", action = "tabnew" },
+              { key = "H", action = "" },
             },
           },
         },
